4e5f19efefd0fba86deabcacfbb29b9399bb6f96
ashley <kilgourkilgour@hotmail.com>
2016-04-26 14:15:44 +0100

    Get all the envelopes in the system

--- SignableReadAPIDemo/Program.cs
+++ SignableReadAPIDemo/Program.cs
@@ -10,20 +10,38 @@ namespace SignableReadAPIDemo
 {
     class Program
     {
-        static string getListEnvelopes = "https://api.signable.co.uk/v1/envelopes?offset=1&limit=1";
+        static string getEnvelopesHeaders = "https://api.signable.co.uk/v1/envelopes?offset=1&limit=1";
+        static string getListEnvelopes = "https://api.signable.co.uk/v1/envelopes?offset={0}&limit={1}";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your API Key from  https://app.signable.co.uk/api");
-            string apiKey =  Console.ReadLine();
+            string apiKey = "99b18594ed0144188a0efe109043702a";//Console.ReadLine();
 
             var webClient = CreateWebClient(apiKey);
 
-           var envelopesHeaderJSON = GetDetail(webClient, getListEnvelopes);
+            var envelopesHeaderJSON = GetDetail(webClient, getEnvelopesHeaders);
 
             JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
             Rootobject envelopesHeader = javaScriptSerializer.Deserialize<Rootobject>(envelopesHeaderJSON);
 
+            int recordsPerPage = 5;
+            int pageCount = (int.Parse(envelopesHeader.total_envelopes) + recordsPerPage - 1) / recordsPerPage;
+
+            var envelopeFingerprints = new List<Guid>();
+            string rootJSON;
+             
+            for (int i = 0; i < pageCount; i++)
+            {
+                rootJSON = GetDetail(webClient, string.Format(getListEnvelopes, i * recordsPerPage, recordsPerPage));
+                Rootobject root = javaScriptSerializer.Deserialize<Rootobject>(rootJSON);
+                foreach (var envelope in root.envelopes)
+                {
+                    envelopeFingerprints.Add(new Guid(envelope.envelope_fingerprint));
+                }
+            }
+
+             
         }
 
         private static WebClient CreateWebClient(string apiKey)
@@ -39,7 +57,7 @@ namespace SignableReadAPIDemo
         private static string GetDetail(WebClient webClient, string apiCommand)
         {
             var results = webClient.DownloadString(apiCommand);
-            Console.WriteLine(results);
+            //Console.WriteLine(results);
             Console.WriteLine("Press any key to continue ...");
             Console.ReadLine();
             return results;
